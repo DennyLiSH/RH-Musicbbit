3ceb252de9ea1be4902828d1f5fef21ec98fcfda
Denny LI Guang <3855135+DennyLiSH@users.noreply.github.com>
2026-04-24 02:41:14 +0800

    feat: add Hilt DataStoreModule for Preferences

---
+++ app/src/main/java/com/rabbithole/musicbbit/di/DataStoreModule.kt
@@ -0,0 +1,28 @@
+package com.rabbithole.musicbbit.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import java.io.File
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create {
+            File(context.filesDir, "datastore/settings.preferences_pb")
+        }
+    }
+}
