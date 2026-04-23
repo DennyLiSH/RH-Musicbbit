95d06b92781895fafa434f9f8a6a17a0f9a2d4f7
Denny LI Guang <3855135+DennyLiSH@users.noreply.github.com>
2026-04-24 02:41:22 +0800

    feat: add Hilt RepositoryModule placeholder

---
+++ app/src/main/java/com/rabbithole/musicbbit/di/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.rabbithole.musicbbit.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    // Repository bindings will be added here when implementations are created
+}
