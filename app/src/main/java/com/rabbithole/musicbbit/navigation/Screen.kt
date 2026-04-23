df9d02ccb7aa9b41ecbe549169bafca0caaf729e
Denny LI Guang <3855135+DennyLiSH@users.noreply.github.com>
2026-04-24 02:42:02 +0800

    feat: add type-safe navigation routes

---
+++ app/src/main/java/com/rabbithole/musicbbit/navigation/Screen.kt
@@ -0,0 +1,15 @@
+package com.rabbithole.musicbbit.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object MusicBrowse
+
+@Serializable
+data object Playlist
+
+@Serializable
+data object Alarm
+
+@Serializable
+data object Player
