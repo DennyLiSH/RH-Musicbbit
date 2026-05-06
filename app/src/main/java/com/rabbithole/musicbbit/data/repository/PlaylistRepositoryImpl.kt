3c75babb5ad0ff20cb3f54fc22c3685818fd50b3
Denny LI Guang <3855135+DennyLiSH@users.noreply.github.com>
2026-05-07 07:33:35 +0800

    fix(playlist): remove unused flow.map import

--- app/src/main/java/com/rabbithole/musicbbit/data/repository/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/rabbithole/musicbbit/data/repository/PlaylistRepositoryImpl.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import javax.inject.Inject
