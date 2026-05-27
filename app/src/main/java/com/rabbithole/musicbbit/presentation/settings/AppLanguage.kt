8ef5c9826a2d4ae860c5dc6e20ead7b981ec2bf3
Denny LI Guang <3855135+DennyLiSH@users.noreply.github.com>
2026-05-27 18:44:21 +0800

    feat: add AppLanguage enum for language selection

---
+++ app/src/main/java/com/rabbithole/musicbbit/presentation/settings/AppLanguage.kt
@@ -0,0 +1,8 @@
+package com.rabbithole.musicbbit.presentation.settings
+
+enum class AppLanguage(val tag: String) {
+    SYSTEM(""),
+    ENGLISH("en"),
+    CHINESE("zh-CN"),
+    JAPANESE("ja")
+}
